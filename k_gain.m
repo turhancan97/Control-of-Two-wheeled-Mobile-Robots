7e9eaebae8c2c5aee0c157305f7676a6cbf9bfb1
turhancan97 <turhancan.kargin@gmail.com>
2022-06-06 14:21:30 +0200

    gain for k is added

--- k_gain.m
+++ k_gain.m
@@ -1,9 +1,5 @@
 %% Gain of the System
-% TODO: add fundtion to find k gain (k1,k2,k3)
-% function k = k_gain(x_r,y_r,th_r, x, y, theta_i)
-% M = [cos(theta_i) sin(theta_i) 0;
-%     -sin(theta_i) cos(theta_i) 0;
-%     0 0 1];
-% c = [x_r - x; y_r - y; th_r - theta_i];
-% e = M * c;
-% end
+% TODO: tune for better results
+function [k1,k2,k3] = k_gain(k1, k2, k3)
+
+end
